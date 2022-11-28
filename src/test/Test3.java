62454646523a74511ad6b057b389c4e3edac575c
Ltianwei <119269329+Ltianwei@users.noreply.github.com>
2022-11-28 10:19:38 +0800

    Update Test3.java

--- src/test/Test3.java
+++ src/test/Test3.java
@@ -4,5 +4,7 @@ public class Test3 {
     public static void main(String[] args) {
         System.out.println("登录模块");
         System.out.println(1);
+         System.out.println(1);
+         System.out.println(1);
     }
 }
